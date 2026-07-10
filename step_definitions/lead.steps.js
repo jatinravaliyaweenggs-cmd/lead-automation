6f3a887ac950e711a66c9abd7e7cab3ff09aa4c0
jatinravaliyaweenggs-cmd <jatin.ravaliya@weenggs.in>
2026-07-10 17:54:52 +0530

    Fix: Lead page code seperate

--- step_definitions/lead.steps.js
+++ step_definitions/lead.steps.js
@@ -8,14 +8,14 @@ When('User navigates to Lead page and opens Add Lead form', async function () {
   this.leadPage = new LeadPage(this.page);
   await this.leadPage.clickDashboard();
   await this.leadPage.clickLeadMenu();
-  await this.leadPage.waitForLeadPage();
-  await this.leadPage.leadsHeading.waitFor({ state: 'visible', timeout: 15000 });
-  await this.leadPage.clickAddLeadButton();
-  await this.leadPage.clickLeadOption();
+
 });
 
 Then('Lead form should be opened', async function () {
-  await this.page.waitForTimeout(2000);
+    await this.leadPage.waitForLeadPage();
+  await this.leadPage.leadsHeading.waitFor({ state: 'visible', timeout: 15000 });
+  await this.leadPage.clickAddLeadButton();
+  await this.leadPage.clickLeadOption();
 });
 
 Then('Lead form fields should have correct placeholders', async function () {
